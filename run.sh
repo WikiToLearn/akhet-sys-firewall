cbfc837ca2e317d1a6cce4d1ca812f8b1f93b57b
Riccardo Iaconelli <riccardo@kde.org>
2015-10-22 01:05:56 +0200

    luca ha rotto tutto
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- run.sh
+++ run.sh
@@ -4,7 +4,17 @@
 # iptables -t filter -A OUTPUT -p udp -j DROP
 # iptables -t filter -A OUTPUT -p icmp -j DROP
 
-sleep 2
+# sleep 1
+
+while true ; do
+ netstat -netupa | grep 5900 &> /dev/null
+ if [[ $? -ne 0 ]] ; then
+  echo "Waiting x11vnc..."
+else
+  break
+ fi
+ sleep 1
+done
 
 while true ; do
  netstat -netupa | grep 5900 &> /dev/null
